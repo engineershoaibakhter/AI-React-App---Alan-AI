baa6f9316efab47164cde1fb43b63d1fc67f9d7f
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-06-11 11:27:19 +0500

    again changing api

--- project/src/Service/GetNews.js
+++ project/src/Service/GetNews.js
@@ -4,5 +4,5 @@ export const GetNews = (category) => {
     const endpoint = process.env.REACT_APP_API_Endpoint;
     const apiKey = process.env.REACT_APP_API_Key;
 
-    return axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=e0374276f2684efcfbc2028327ba8d002e956eca572e1d8b807a3e2338fdd0dc/stage`);
+    return axios.get(`${endpoint}&category=${category}&apiKey=${apiKey}`);
 };
